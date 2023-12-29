f63bc5ed51503491e64dded4a0eb40394bcc4f63
FRANCOHAILIMITIDELLEVOLUTO <121171151+FRANCO8724@users.noreply.github.com>
2023-12-29 19:51:22 +0100

    Creazione form

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,45 @@ namespace _13___interfacce
 {
     public partial class Form1 : Form
     {
+        double Totale;
+        string Descrizione;
         public Form1()
         {
             InitializeComponent();
+            Totale = 0;
+            Descrizione = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Aliante aliante = new Aliante(textBox1.Text, textBox2.Text, textBox3.Text);
+            Ala ala = new Ala(Convert.ToInt32(textBox4.Text), Convert.ToInt32(textBox5.Text));
+            Fusoliera fusoliera = new Fusoliera(Convert.ToInt32(textBox6.Text), textBox7.Text);
+            Coda coda = new Coda(Convert.ToInt32(textBox8.Text));
+            Ruota ruota = new Ruota(Convert.ToInt32(textBox9.Text), Convert.ToInt32(textBox10.Text), Convert.ToInt32(textBox11.Text), Convert.ToInt32(textBox12.Text));
+
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+            textBox7.Text = "";
+            textBox8.Text = "";
+            textBox9.Text = "";
+            textBox10.Text = "";
+            textBox11.Text = "";
+            textBox12.Text = "";
+
+
+            Totale = ala.SommaCosti() + fusoliera.SommaCosti() + coda.SommaCosti() + ruota.SommaCosti();
+            Descrizione = aliante.Descrizione() + fusoliera.Descrizione();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Add("Totale: " + Convert.ToString(Totale));
+            listView1.Items.Add("Descrizione: " + Descrizione);
         }
     }
 }
